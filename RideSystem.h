652de168fb6c763a203886b6450c762fc016df57
sharmakrishna02005-commits <sharmakrishna02005@gmail.com>
2026-07-17 18:07:53 +0530

    Update RideSystem.h

--- RideSystem.h
+++ RideSystem.h
@@ -16,32 +16,32 @@ private:
     Queue waitingQueue;
     std::unordered_map<int, Passenger*> activeRides; // Tracks driverId -> Passenger*
 
-    // Helper methods to find passenger and driver by ID
+    
     Passenger* findPassenger(int id) const;
     Driver* findDriver(int id) const;
 
 public:
-    // Constructor
+    
     RideSystem();
 
-    // Destructor
+    
     ~RideSystem();
 
-    // Registration
+    
     void registerDriver(int id, const std::string& name, double x, double y, double rating = 5.0);
     void registerPassenger(int id, const std::string& name, double x, double y, double destX, double destY);
 
-    // Ride operations
+    
     void requestRide(int passengerId);
     void completeRide(int driverId);
 
-    // Getters
+    
     const std::vector<Driver*>& getDrivers() const;
     const std::vector<Passenger*>& getPassengers() const;
     const Queue& getWaitingQueue() const;
 
-    // Display system status
+    
     void displayStatus() const;
 };
 
-#endif // RIDESYSTEM_H
+#endif 

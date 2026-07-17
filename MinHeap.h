9b1fa2e6f49511ee071729097a84024f38b36cef
sharmakrishna02005-commits <sharmakrishna02005@gmail.com>
2026-07-17 18:01:52 +0530

    Refactor MinHeap class header for clarity

--- MinHeap.h
+++ MinHeap.h
@@ -15,18 +15,18 @@ class MinHeap {
 private:
     std::vector<HeapNode> heap;
 
-    // Helper functions to maintain heap properties
+    
     void heapifyUp(int index);
     void heapifyDown(int index);
 
 public:
-    // Constructor
+    
     MinHeap();
 
-    // Destructor
+    
     ~MinHeap();
 
-    // Heap operations
+    
     void insert(Driver* driver, double distance);
     Driver* extractMin();
     Driver* getMin() const;
@@ -35,4 +35,4 @@ public:
     void clear();
 };
 
-#endif // MINHEAP_H
+#endif 
